37e51ea41d24f4439ef0fa7d9d705ae45eb69884
= <andrii.marusiak@gmail.com>
2015-11-19 17:33:43 +0200

    Database Codefirst

---
+++ Menu/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Menu
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+            : base("name = DatabaseContext")
+        { 
+            Database.SetInitializer<DatabaseContext> (new DBInitializer());
+        }
+
+        public virtual DbSet<Person> Persons { get; set; }
+
+
+    }
+
+    public class DBInitializer : DropCreateDatabaseAlways<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
