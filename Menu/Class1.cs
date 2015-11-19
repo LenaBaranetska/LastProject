ae319276588c0f0767da54597e2d505edfa17eea
Олександр Гузік <guzik@step.itstep.rv.ua>
2015-11-19 16:21:32 +0200

    Created new project

---
+++ Menu/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+   public static class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Menu");
+        }
+    }
+}
