b5514a4689219b44ad40a522716a458a57a645ab
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-11-19 16:36:20 +0200

    added person class

---
+++ Menu/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+	class Person
+	{
+		string Name { set; get; }
+		string Surname { set; get; }
+
+		Person()
+		{
+			Console.WriteLine("input name: ");
+			Name = Console.ReadLine();
+
+			Console.WriteLine("input Surname: ");
+			Surname = Console.ReadLine();
+		}
+
+		public void Print()
+		{
+			Console.WriteLine("{0} : {1}", Name, Surname);
+		}
+
+	}
+}
